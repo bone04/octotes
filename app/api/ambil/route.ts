b73a6edfe6d160c0f1ae5968015d0da3a04c1710
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 11:20:12 +0800

    Change default GitHub repo name to 'octotes'

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -7,7 +7,7 @@ export const dynamic = 'force-static';
 export async function GET() {
   
   const owner: string = process.env.GITHUB_OWNER || "bone04"
-  const repo: string = process.env.GITHUB_REPO || "learn-octo"
+  const repo: string = process.env.GITHUB_REPO || "octotes"
   const path: string = process.env.GITHUB_FILE_PATH || "items.json"
  if (!owner) {
       throw new Error("OWNER environment variable is required.");
