44ce34a0cbe51b3442018a0ddce033abef5eb9bf
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 22:25:48 +0800

    Implement GitHub API request in route handler
    Added functionality to make a REST API request to GitHub and process the response.

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -14,7 +14,32 @@ export async function GET() {
       auth: process.env.GITHUB_TOKEN || undefined
     });
   
-  return NextResponse.json({
-    hello: "GeeksforGeeks",
-  });
+  // Make a REST API request
+ const  { data }  = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+  owner,
+  repo,
+  path,
+  headers: {
+    'X-GitHub-Api-Version': '2026-03-10'
+  }
+})
+  
+  
+  if (!Array.isArray(data)) return [];
+  
+  /*
+  if (!Array.isArray(data)) { // Filter out directories
+
+    if (data.type === 'file') { // Make sure it's a file
+      data.content;
+    }
+  }
+  */
+  /*
+    if (!Array.isArray(data) && data.content) {
+      const content = Buffer.from(data.content, "base64").toString("utf8");
+      console.log(content);
+      }
+  */
+return NextResponse.json({ message: "Data received" });
 }
