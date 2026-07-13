5499a34b2c2559073cc0fd9fae7592534f9f81ff
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 11:56:22 +0800

    Add GET route to return a hello world message

---
+++ app/api/ambil/route.ts
@@ -0,0 +1,9 @@
+// app/api/hello/route.ts
+import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
+
+export async function GET() {
+  const data = { message: "Hello, world!" };
+  
+  return NextResponse.json(data);
+}
