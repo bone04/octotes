0370323100a460bf8138db1332da2901591aef7d
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 11:32:23 +0800

    Refactor Octokit import and API request method
    Updated the Octokit import and modified the API request method to use the new syntax.

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -1,6 +1,8 @@
 
 import { NextResponse } from "next/server";
-import { Octokit } from "@octokit/rest";
+// import { Octokit } from "@octokit/rest";
+import { Octokit } from "@octokit/core";
+
 export const dynamic = 'force-static';
 // https://raw.githubusercontent.com/bone04/learn-octo/main/items.json
 
@@ -15,12 +17,16 @@ export async function GET() {
   const octokit = new Octokit({
       auth: process.env.GITHUB_TOKEN || undefined
     });
-    
-  const  { data } = await octokit.rest.repos.getContent({
-      owner,
-      repo,
-      path
-    });
+   // Make a REST API request
+ const  { data }  = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+  owner: 'OWNER',
+  repo: 'REPO',
+  path: 'PATH',
+  headers: {
+    'X-GitHub-Api-Version': '2026-03-10'
+  }
+})
+  
   
   if (!Array.isArray(data)) return [];
   
