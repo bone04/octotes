e7b6b07672ebb472966dfd33a110ea750cbbe336
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 22:10:45 +0800

    Configure GitHub repo details in GET function
    Added GitHub repository configuration and error handling.

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server";
+import { Octokit } from "@octokit/core";
+
 export const dynamic = 'force-static'
 
 export async function GET() {
+  const owner: string = process.env.GITHUB_OWNER || "bone04"
+  const repo: string = process.env.GITHUB_REPO || "octotes"
+  const path: string = process.env.GITHUB_FILE_PATH || "items.json"
+ if (!owner) {
+      throw new Error("OWNER environment variable is required.");
+    }
+  const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || undefined
+    });
+  
   return NextResponse.json({
     hello: "GeeksforGeeks",
   });
