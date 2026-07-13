895dba026c0c136276e4c0ac2eecf944a243d0b0
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 11:54:17 +0800

    Delete app/api/ambil/route.ts

--- app/api/ambil/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/hello/route.ts
-import { NextResponse } from 'next/server';
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-  const data = { message: "Hello, world!" };
-  
-  return NextResponse.json(data);
-}
