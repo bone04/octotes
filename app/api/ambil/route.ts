355406f3bd5e9b91d82e4e6427cabbb6b98456fc
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 11:48:13 +0800

    Add dynamic export to route.ts

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -1,5 +1,6 @@
 // app/api/hello/route.ts
 import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   const data = { message: "Hello, world!" };
