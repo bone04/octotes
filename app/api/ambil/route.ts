7c2cc90d0e487e8870caa34eca81ed8fdb798d50
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 10:37:05 +0800

    Implement GET method to retrieve GitHub content
    Fetch content from GitHub repository using Octokit.

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -1,1 +1,42 @@
 
+import { NextResponse } from "next/server";
+import { Octokit } from "@octokit/rest";
+export const dynamic = 'force-static';
+// https://raw.githubusercontent.com/bone04/learn-octo/main/items.json
+
+export async function GET() {
+  
+  const owner: string = process.env.GITHUB_OWNER || "bone04"
+  const repo: string = process.env.GITHUB_REPO || "learn-octo"
+  const path: string = process.env.GITHUB_FILE_PATH || "items.json"
+ if (!owner) {
+      throw new Error("OWNER environment variable is required.");
+    }
+  const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || undefined
+    });
+    
+  const  { data } = await octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path
+    });
+  
+  if (!Array.isArray(data)) return [];
+  
+  /*
+  if (!Array.isArray(data)) { // Filter out directories
+
+    if (data.type === 'file') { // Make sure it's a file
+      data.content;
+    }
+  }
+  */
+  /*
+    if (!Array.isArray(data) && data.content) {
+      const content = Buffer.from(data.content, "base64").toString("utf8");
+      console.log(content);
+      }
+  */
+return NextResponse.json({ message: "Data received" });
+}
