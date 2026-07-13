0fd0b62b3018b38d400c39c684ab0bad38c2aa0b
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-13 21:56:30 +0800

    Update route.ts

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -1,12 +1,1 @@
 import { NextResponse } from 'next/server';
-
-// Handle the GET request
-export async function GET() {
-  const users = [
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-  ];
-
-  // Return the data as JSON
-  return NextResponse.json(users);
-}
