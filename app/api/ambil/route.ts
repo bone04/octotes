07b5bf4f72ae82791930ac36af1a393f80a3f039
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-14 22:51:13 +0800

    Refactor data validation in route handler

--- app/api/ambil/route.ts
+++ app/api/ambil/route.ts
@@ -24,10 +24,14 @@ export async function GET() {
     }
   })
 
-  if (!Array.isArray(data) && data.content) {
+ if (!Array.isArray(data)) { // Filter out directories
+
+    if (data.type === 'file') { // Make sure it's a file
+      // data.content;
       const content = Buffer.from(data.content, "base64").toString("utf8");
       console.log(content);
     }
+  }
 
 return NextResponse.json({ message: "Data received" });
 }
