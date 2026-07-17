d540b155549b46f1a40a8e934040d9ce485c8fc4
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-17 23:12:39 +0800

    Create CorezPage component to fetch GitHub data

---
+++ app/corez/page.tsx
@@ -0,0 +1,42 @@
+export default function CorezPage() {
+  const owner: string = process.env.GITHUB_OWNER || "bone04"
+  const repo: string = process.env.GITHUB_REPO || "octotes"
+  const path: string = process.env.GITHUB_FILE_PATH || "items.json"
+ if (!owner) {
+      throw new Error("OWNER environment variable is required.");
+    }
+  const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || undefined
+    });
+  
+  // Make a REST API request
+  try {
+       const  result   = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+          owner,
+          repo,
+          path,
+          headers: {
+            'X-GitHub-Api-Version': '2026-03-10'
+          },
+         mediaType: {
+            format: "raw", 
+          },
+        })
+    // console.log(result.type)
+  //  console.log(Array.isArray(result));
+ // console.log(`Success! Status: ${result.status}. Rate limit remaining: ${result.headers["x-ratelimit-remaining"]}`)
+    // const content = Buffer.from(result._links.self, 'base64').toString() // result.data.content
+    // console.log(content)
+
+} catch (err) {
+    if (typeof err === 'object' && err !== null) {
+      console.log(err.toString());
+    } else {
+      console.log('Unexpected error', err);
+    }
+  // console.log(`Error! Status: ${error.status}. Rate limit remaining: ${error.headers["x-ratelimit-remaining"]}. Message: ${error.response.data.message}`)
+}
+  //message: "Data received"
+
+  return ()
+}
