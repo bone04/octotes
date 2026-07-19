18ebb3ace5e12f741c3ca0da406bbb96ceb7ece8
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-19 11:27:42 +0800

    Comment out rate limit console log
    Comment out console log for rate limit status.

--- app/corez/page.tsx
+++ app/corez/page.tsx
@@ -26,7 +26,7 @@ export default async function CorezPage() {
         })
     // console.log(result.type)
  
- console.log(`Success! Status: ${result.status}. Rate limit remaining: ${result.headers["x-ratelimit-remaining"]}`)
+// console.log(`Success! Status: ${result.status}. Rate limit remaining: ${result.headers["x-ratelimit-remaining"]}`)
     // const content = Buffer.from(result._links.self, 'base64').toString() // result.data.content
     // console.log(content)
 
