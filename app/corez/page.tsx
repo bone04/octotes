2de3bf5aa16be281a2d13c9b2121e97e95070b2d
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-19 12:56:24 +0800

    Comment out API request code in page.tsx
    Comment out the REST API request code block for future reference.

--- app/corez/page.tsx
+++ app/corez/page.tsx
@@ -12,7 +12,7 @@ export default async function CorezPage() {
     });
   
   // Make a REST API request
-  
+  /*
        const  result   = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
           owner,
           repo,
@@ -25,7 +25,7 @@ export default async function CorezPage() {
           },
         })
     // console.log(result.type)
- 
+ */
 // console.log(`Success! Status: ${result.status}. Rate limit remaining: ${result.headers["x-ratelimit-remaining"]}`)
     // const content = Buffer.from(result._links.self, 'base64').toString() // result.data.content
     // console.log(content)
