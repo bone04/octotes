5135b403cadf5ae62ea30ecc2a590835cdf0789b
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-30 16:15:28 +0800

    Import React alongside useState in page.tsx

--- app/pissrest/page.tsx
+++ app/pissrest/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 // import { Octokit } from "@octokit/rest";
-import { useState } from "react";
+import React, { useState } from "react";
 
 interface FileFormProps {
   onSubmit: (data: {
