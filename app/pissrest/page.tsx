da5b912f961616695e32bd851df0af4bec7d118a
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-07-31 21:27:21 +0800

    Update fetch URL syntax to template literals

--- app/pissrest/page.tsx
+++ app/pissrest/page.tsx
@@ -30,7 +30,7 @@ export default function PissRestPage() {
     }
     // onSubmit({ token, owner, repo, path });
     try {
-      const response = await fetch("https://bone04.github.io/octotes/api/pissrest", {
+      const response = await fetch(`https://bone04.github.io/octotes/api/pissrest`, {
         method: "PUT",
         headers: {
           'Authorization': 'Bearer YOUR_GITHUB_TOKEN',
