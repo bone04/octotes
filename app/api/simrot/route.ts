50054fb282bcaaf4549142fc86b5a8a03095a265
Bone4Coder <88817100+bone04@users.noreply.github.com>
2026-08-02 22:14:44 +0800

    Enable dynamic static generation for route

--- app/api/simrot/route.ts
+++ app/api/simrot/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
- 
+
+export const dynamic = "force-static"
+
 export async function POST(request: Request) {
   return NextResponse.json({ message: "Hello from Next.js!" });
 }
